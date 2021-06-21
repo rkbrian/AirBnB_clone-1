0372574256cdc5dae5b420cb885b498a3bd3fe72
rkbrian <rkbriankong@gmail.com>
2021-06-21 15:36:01 +0000

    6-passing alternate contents to html

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application."""
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_hbnb():
+    """text hello hbnb!"""
+
+    strict_slashes = False
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hello_again():
+    """text hbnb"""
+
+    strict_slashes = False
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def cis_alwaysfun(text):
+    """text c and something"""
+
+    strict_slashes = False
+    alt_string = text.replace('_', ' ')
+    return 'C {}'.format(alt_string)
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def pythonis_cool(text='is cool'):
+    """text python and something"""
+
+    strict_slashes = False
+    alt_string = text.replace('_', ' ')
+    return 'Python {}'.format(alt_string)
+
+
+@app.route('/number/<int:n>')
+def numberis_num(n):
+    """text number and something"""
+
+    strict_slashes = False
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>')
+def numbertemplate(n):
+    """text number and something"""
+
+    strict_slashes = False
+    return render_template('5-number.html', getnum=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def even_or_odd(n):
+    """text number and something"""
+
+    strict_slashes = False
+    if n % 2 == 0:
+        return render_template('6-number_odd_or_even.html', num=n, eost='even')
+    else:
+        return render_template('6-number_odd_or_even.html', num=n, eost='odd')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
