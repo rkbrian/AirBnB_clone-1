f8af6cc290b427ce5ee879a15c51c846ef1e2ffa
rkbrian <rkbriankong@gmail.com>
2021-06-21 03:27:00 +0000

    fix typo on file 0; file 1 - 2 functions for different displays

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,7 +10,14 @@ def hello_hbnb():
     """web application to listen on 0.0.0.0, port 5000"""
 
     strict_slashes = False
-    return 'Hello HBNB!'
+    return 'Hello, HBNB!'
+
+@app.route('/hbnb')
+def hello_again():
+    """web application to listen on 0.0.0.0, port 5000"""
+
+    strict_slashes = False
+    return 'HBNB'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
